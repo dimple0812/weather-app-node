29e7a1eea4c4089b254fa83097705968ac5b2fb6
Dimple Sharma <dimplesharma@Dimples-MacBook-Pro.local>
2021-01-08 11:22:23 +0530

    First commit

---
+++ utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+const forcast = ({latitude, longitude, location}, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=dd675302ed91ae3dccc9633f5ba4b960&query='+ encodeURIComponent(latitude.toString()) + ',' + encodeURIComponent(longitude.toString()) + '&units=f'
+    request({url: url, json: true}, (err, response) => {
+        if(err){
+            callback("Unable to connect to weatherstack api", undefined)
+        }
+        else if(response.body.success === false){
+            callback("Invalide value of latitude and longitude", undefined)
+        }
+        else{
+            const data = response.body
+            const {temperature, feelslike} = data.current
+            const response_str = "It is currently " + data.current.temperature + " degree F. It feels like " + data.current.feelslike + " degree F. Weather description: " + data.current.weather_descriptions[0]
+            callback(undefined, response_str)
+            
+            
+        }
+        
+        
+    })
+
+}
+
+module.exports = forcast;
---
+++ utils/geocode.js
@@ -0,0 +1,31 @@
+const request = require('request')
+
+const geoCode = (address, callback ) => {
+
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=pk.eyJ1IjoiZGltcGxlc2hhcm1hMDgxMiIsImEiOiJja2l1ZWFqMzQwOHozMnptZW04MmpiaXJ6In0.7WrVOe3Giv5gCOycKDIp8Q&limit=1"
+    
+    request({url: url, json: true},(err,response) => {
+        
+        if(err){
+            callback("Unable to connect to API", undefined)
+        }
+        else if(response.body.message === "Not Found"){
+            callback("Invalid City parameter passed", undefined) 
+        }
+        else{
+
+            let data = response.body.features[0].center
+            callback(undefined, {
+                latitude: data[1],
+                longitude: data[0],
+                location: response.body.features[0].place_name
+            })
+
+            
+        }
+    })
+
+
+}
+
+module.exports = geoCode
